36315ed30572d2b4e587beb43d9ef29d4cee3bba
sravyasridivakarla <sravyad@gmail.com>
2019-03-28 01:59:52 -0700

    we made classes cuz thats imp

--- graphics/HelloGraphics.cpp
+++ graphics/HelloGraphics.cpp
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
+using namespace std;
 
 
 float *PixelBuffer;
@@ -11,25 +13,54 @@ int WIDTH = 200;
 int HEIGHT =  200;
 void display();
 
+class Point{
+public:
+    //coordinate
+    int x;
+    int y;
+    //color
+    int r;
+    int g;
+    int b;
+    
+    //default rgb
+    Point(int ex, int why){
+        x = ex;
+        y = why;
+        r = 255;
+        g = 255;
+        b = 255;
+    }
+    
+    //custom rgb
+    Point(int ex, int why, int are, int gee, int bee){
+        x = ex;
+        y = why;
+        r = are;
+        g = gee;
+        b = bee;
+    }
+};
 
 
-void makePixel(int x, int y, int r, int g, int b, float* pixels, int width, int height)
+
+void makePixel(Point p, float* pixels, int width, int height)
 {
-    if (0 <= x && x < width && 0 <= y && y < height) {
-        int position = (x + y * width) * 3;
-        pixels[position] = r;
-        pixels[position + 1] = g;
-        pixels[position + 2] = b;
+    if (0 <= p.x && p.x < width && 0 <= p.y && p.y < height) {
+        int position = (p.x + p.y * width) * 3;
+        pixels[position] = p.r;
+        pixels[position + 1] = p.g;
+        pixels[position + 2] = p.b;
     }
 }
 
-void drawLineDDA(int x0, int y0, int x1, int y1){
+void drawLineDDA(Point p0, Point p1){
     
     int steps;
     
     //Calculate the difference b/w the two end points
-    int dx = x1 - x0;
-    int dy = y1 - y0;
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
     
     //find out which axis has more steps
     if( abs(dx) > abs(dy) ) {
@@ -37,63 +68,67 @@ void drawLineDDA(int x0, int y0, int x1, int y1){
     } else {
         steps = abs(dy);
     }
-    
+
     //Calculate the increment in x coordinate and y coordinate
     int Xinc = dx / (float) steps;
     int Yinc = dy / (float) steps;
     
-    //initialize the starting points
-    int x=x0;
-    int y=y0;
-    
+    //initialize the starting point
+    Point p(p0.x, p0.y);
+    cout << p.x << " " << p.y ;
+
     //draw the line step by step by incrementing the x and y axis
     for(int v = 0; v < steps; v++)
     {
-        x = x + Xinc;
-        y = y + Yinc;
-        makePixel(round(x), round(y),255, 255, 255, PixelBuffer, WIDTH, HEIGHT);
+        p.x = p.x + Xinc;
+        p.y = p.y + Yinc;
+        
+        p.x = round(p.x);
+        p.y = round(p.y);
+        makePixel(p, PixelBuffer, WIDTH, HEIGHT);
     }
 }
 
 
 
-void drawLineBresenham(int x0, int y0, int x1, int y1)
+void drawLineBresenham(Point p0, Point p1)
 {
     
     // distance
-    int dx = (x1-x0);
-    int dy = (y1-y0);
+    int dx = (p1.x - p0.x);
+    int dy = (p1.y - p0.y);
     
     //initialize the starting points of line
-    int x=x0;
-    int y=y0;
+    Point p(p0.x, p0.y);
+    
  
     //initial value of slope_error
-    int p=2*dy-dx;
+    int e =2*dy-dx;
  
-    while(x<x1){
-        if(p>=0){
-            makePixel(x,y,255,255,255,PixelBuffer, WIDTH, HEIGHT);
-            y=y+1;
-            p=p+2*dy-2*dx;
+    while(p.x < p1.x){
+        if(e >=0){
+            makePixel(p, PixelBuffer, WIDTH, HEIGHT);
+            p.y = p.y+1;
+            e = e + 2 * dy-2 * dx;
         } else {
-            makePixel(x,y,255,255,255,PixelBuffer, WIDTH, HEIGHT);
-            p=p+2*dy;
+            makePixel(p, PixelBuffer, WIDTH, HEIGHT);
+            e = e + 2 * dy;
         }
-        x=x+1;
+        p.x = p.x + 1;
     }
  }
 
-void drawLine(int x0, int y0, int x1, int y1, int choice){
+void drawLine(Point p0, Point p1, int choice){
     // 0 - drawLineDDA(int x0, int y0, int x1, int y1)
     // 1 - drawlineBresenham(int x0, int y0, int x1, int y1)
     
     if( choice == 0){
-        drawLineDDA(x0, y0, x1, y1);
+        drawLineDDA(p0, p1);
     } else {
-        drawLineBresenham(x0, y0, x1, y1);
+        drawLineBresenham(p0, p1);
     }
 }
+
 
 
 
@@ -114,8 +149,10 @@ int main(int argc, char *argv[])
     glClearColor(0, 0, 0, 0); //clears the buffer of OpenGL
     
     //draw the pixel with the right color
-    //makePixel(100,100,255,255,255,PixelBuffer, WIDTH, HEIGHT);
-    drawLine(50, 50, 100, 100,0);
+    Point p1(50,50);
+    Point p2(150,150);
+    makePixel(p1,PixelBuffer, WIDTH, HEIGHT);
+    drawLine(p1,p2,1);
     
     
     //sets display function
