eee3c1e8b337775c3d32693cc5985517768c58dd
srd456 <srdennis@ucdavis.edu>
2019-03-28 17:26:08 -0700

    added global variables

--- graphics/HelloGraphics.cpp
+++ graphics/HelloGraphics.cpp
@@ -9,19 +9,28 @@ using namespace std;
 
 
 float *PixelBuffer;
-int WIDTH = 200;
-int HEIGHT =  200;
+const int WINDOW_SIZE_WIDTH = 200;
+const int WINDOW_SIZE_HEIGHT = 200;
+const int WINDOW_SIZE_POS_X = 100;
+const int WINDOW_SIZE_POS_Y = 100;
+
+const int WIDTH = 200;
+const int HEIGHT =  200;
+
+const int CORD_X0 = 50;
+const int CORD_Y0 = 50;
+const int CORD_X1 = 150;
+const int CORD_Y1 = 150;
+
+
 void display();
 
 class Point{
 public:
-    //coordinate
-    int x;
-    int y;
-    //color
-    int r;
-    int g;
-    int b;
+    //Coordinate
+    int x, y;
+    //Color
+    int r, g, b;
     
     //default rgb
     Point(int ex, int why){
@@ -59,29 +68,30 @@ void drawLineDDA(Point p0, Point p1){
     int steps;
     
     //Calculate the difference b/w the two end points
-    int dx = p1.x - p0.x;
-    int dy = p1.y - p0.y;
+    //IS THIS FLOAT????
+    float dx = p1.x - p0.x;
+    float dy = p1.y - p0.y;
     
-    //find out which axis has more steps
+    //Find out which axis has more steps
     if( abs(dx) > abs(dy) ) {
         steps = abs(dx);
     } else {
         steps = abs(dy);
     }
-
+    
     //Calculate the increment in x coordinate and y coordinate
-    int Xinc = dx / (float) steps;
-    int Yinc = dy / (float) steps;
+    int xInc = dx / (float) steps;
+    int yInc = dy / (float) steps;
     
     //initialize the starting point
     Point p(p0.x, p0.y);
     cout << p.x << " " << p.y ;
-
+    
     //draw the line step by step by incrementing the x and y axis
     for(int v = 0; v < steps; v++)
     {
-        p.x = p.x + Xinc;
-        p.y = p.y + Yinc;
+        p.x = p.x + xInc;
+        p.y = p.y + yInc;
         
         p.x = round(p.x);
         p.y = round(p.y);
@@ -93,30 +103,28 @@ void drawLineDDA(Point p0, Point p1){
 
 void drawLineBresenham(Point p0, Point p1)
 {
-    
-    // distance
+     //Calculate the difference b/w the two end points
     int dx = (p1.x - p0.x);
     int dy = (p1.y - p0.y);
     
     //initialize the starting points of line
     Point p(p0.x, p0.y);
     
- 
     //initial value of slope_error
-    int e =2*dy-dx;
- 
+    int slopeError = 2 * dy - dx;
+    
     while(p.x < p1.x){
-        if(e >=0){
+        if( slopeError >= 0 ){
             makePixel(p, PixelBuffer, WIDTH, HEIGHT);
             p.y = p.y+1;
-            e = e + 2 * dy-2 * dx;
+            slopeError = slopeError + 2 * dy-2 * dx;
         } else {
             makePixel(p, PixelBuffer, WIDTH, HEIGHT);
-            e = e + 2 * dy;
+            slopeError = slopeError + 2 * dy;
         }
         p.x = p.x + 1;
     }
- }
+}
 
 void drawLine(Point p0, Point p1, int choice){
     // 0 - drawLineDDA(int x0, int y0, int x1, int y1)
@@ -130,8 +138,6 @@ void drawLine(Point p0, Point p1, int choice){
 }
 
 
-
-
 int main(int argc, char *argv[])
 {
     //allocate new pixel buffer, need initialization!!
@@ -139,21 +145,21 @@ int main(int argc, char *argv[])
     
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
-    //set window size to 200*200
-    glutInitWindowSize(200, 200);
-    //set window position
-    glutInitWindowPosition(100, 100);
+    //Set window size
+    glutInitWindowSize(WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT);
+    //Set window position
+    glutInitWindowPosition(WINDOW_SIZE_POS_X, WINDOW_SIZE_POS_Y);
     
     //create and set main window title
     int MainWindow = glutCreateWindow("Hello Graphics!!");
     glClearColor(0, 0, 0, 0); //clears the buffer of OpenGL
     
     //draw the pixel with the right color
-    Point p1(50,50);
-    Point p2(150,150);
+    //p1(x0,y0) p2(x1,y1)
+    Point p1(CORD_X0, CORD_Y0);
+    Point p2(CORD_X1, CORD_Y1);
     makePixel(p1,PixelBuffer, WIDTH, HEIGHT);
     drawLine(p1,p2,1);
-    
     
     //sets display function
     glutDisplayFunc(display);
@@ -169,9 +175,8 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
     //draws pixel on screen, width and height must match pixel buffer dimension
-    glDrawPixels(200, 200, GL_RGB, GL_FLOAT, PixelBuffer);
+    glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_FLOAT, PixelBuffer);
     
     //window refresh
     glFlush();
 }
-
